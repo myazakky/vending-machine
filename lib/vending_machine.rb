c4f482514954256e4d6d6f6df13e2a0552af5b73
nimuc <nimuC@outlook.com>
2020-02-03 02:36:10 +0900

    Make it possible to return any beverage if the vending machine has that

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -2,24 +2,36 @@
 
 class VendingMachine
   attr_reader :money
+  attr_reader :beverages
 
-  def initialize(money: 0)
+  def initialize(money: 0, beverages: [])
     @money = money
+    @beverages = beverages
   end
 
-  def push
-    if @money == 100
-      'cola'
+  def push(beverage_name)
+    if @money == 100 && @beverages.include?(beverage_name)
+      beverage_name
     else
       nil
     end
   end
 
   def insert(money)
-    VendingMachine.new(money: @money + money)
+    VendingMachine.new(
+      money: @money + money,
+      beverages: @beverages
+    )
+  end
+
+  def add_beverage(beverage)
+    VendingMachine.new(
+      money: @money,
+      beverages: @beverages + [beverage]
+    )
   end
 
   def ==(other)
-    @money == other.money
+    (@money == other.money) && (@beverages.sort == other.beverages.sort)
   end
 end
--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -8,17 +8,20 @@ RSpec.describe VendingMachine do
 
   context 'push button' do
     it 'return nil' do
-      expect(vending_machine.push).to be_nil
-    end
-
-    it 'return cola if inserted 100' do
-      inserted_vending_machine = vending_machine.insert(100)
-      expect(inserted_vending_machine.push).to eq 'cola'
+      expect(vending_machine.push('')).to be_nil
     end
 
     it 'return nil if inserted outside of 100' do
       inserted_vending_machine = vending_machine.insert(200)
-      expect(inserted_vending_machine.push).to be_nil
+      expect(inserted_vending_machine.push('')).to be_nil
+    end
+
+    it 'return nil if a vending machine dont has an argument beverage' do
+      expect(vending_machine.insert(100).push('烏龍茶')).to be_nil
+    end
+
+    it 'return 烏龍茶 if a vending machine has 烏龍茶' do
+      expect(vending_machine.add_beverage('烏龍茶').insert(100).push('烏龍茶')).to eq '烏龍茶'
     end
   end
 
@@ -26,6 +29,34 @@ RSpec.describe VendingMachine do
     it 'create vending machine object' do
       expected_vending_machine = VendingMachine.new(money: 100)
       expect(vending_machine.insert(100)).to eq expected_vending_machine
+    end
+  end
+
+  context 'add beverages' do
+    it 'create a vending machine object added the beverages' do
+      expected_vending_machine = VendingMachine.new(beverages: ['烏龍茶'])
+
+      expect(vending_machine.add_beverage('烏龍茶')).to eq expected_vending_machine
+    end
+  end
+
+  context 'compare VendingMachine objects' do
+    it 'false if each the money different' do
+      v1 = VendingMachine.new(money: 0)
+      v2 = VendingMachine.new(money: 1)
+      expect(v1 == v2).to be false
+    end
+
+    it 'false if each the beverages different' do
+      v1 = VendingMachine.new(beverages: [])
+      v2 = VendingMachine.new(beverages: ['monster'])
+      expect(v1 == v2).to be false
+    end
+
+    it 'true if each the beverages sequence different' do
+      v1 = VendingMachine.new(beverages: %w[a b])
+      v2 = VendingMachine.new(beverages: %w[b a])
+      expect(v1 == v2).to be true
     end
   end
 end
